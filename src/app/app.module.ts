f9b9d2923eb529cd7dd72674a3eb96ad5ad9a3f8
Djalma Beraldo Batista <Djalma.Batista@indt.org.br>
2017-10-20 15:51:48 -0400

    Second commit - Setting review and menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,12 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import {RestaurantsService} from './restaurants/restaurants.service'
+import {RestaurantsService} from './restaurants/restaurants.service';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 
 
 @NgModule({
@@ -20,7 +25,12 @@ import {RestaurantsService} from './restaurants/restaurants.service'
     HomeComponent,
     AboutComponent,
     RestaurantsComponent,
-    RestaurantComponent
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    ShoppingCartComponent,
+    MenuItemComponent,
+    MenuComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/restaurants/restaurant/restaurant.model.ts
@@ -0,0 +1,7 @@
+export interface Restaurant {
+    id: string
+    name: string
+    category: string
+    deliveryEstimate: string
+    imagePath: string
+}
