b5428b260ebc47ea87d9f73c5ef92215b2299624
Djalma Beraldo Batista <djalmaberaldo.batista@gmail.com>
2018-02-14 14:35:39 -0400

    ObservableValues from Reactive Form
    Signed-off-by: Djalma Beraldo Batista <djalmaberaldo.batista@gmail.com>

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,7 +1,11 @@
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { trigger, state, style, transition, animate} from '@angular/animations'
 import {Restaurant} from './restaurant/restaurant.model'
 import {RestaurantsService} from './restaurants.service'
+import 'rxjs/add/operator/switchMap';
+
+
 
 @Component({
   selector: 'mt-restaurants',
@@ -21,15 +25,29 @@ import {RestaurantsService} from './restaurants.service'
     ])
   ]
 })
+
 export class RestaurantsComponent implements OnInit {
 
   searchBarState = 'hidden';
-
   restaurants: Restaurant []
 
-  constructor(private restaurantsService: RestaurantsService) { }
+  searchForm: FormGroup;
+  searchControl: FormControl;
+
+  constructor(private restaurantsService: RestaurantsService,
+              private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    this.searchControl = this.fb.control('');
+    this.searchForm = this.fb.group({
+      searchControl: this.searchControl
+    })
+
+    this.searchControl.valueChanges.switchMap(searchTerm =>
+          this.restaurantsService.restaurants(searchTerm))
+          .subscribe(restaurants => this.restaurants = restaurants);
+
     this.restaurantsService.restaurants()
     .subscribe(restaurants => this.restaurants = restaurants)
   }
